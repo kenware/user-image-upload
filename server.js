694f818a0141beb5c7119a12e04640380f776370
kenware <ejykken@gmail.com>
2021-05-30 12:25:44 +0100

    add changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import path from 'path';
 import fileUpload from 'express-fileupload';
 import logger from './server/utils/logger';
 import Route from './server/routes';
@@ -19,6 +20,9 @@ app.use(express.urlencoded({
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 app.use('/api/v1', Route);
 const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, '/client')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './client/index.html')));
 app.listen(PORT, () => logger.info(`App is running at port ${PORT}`));
 
 export default app;
