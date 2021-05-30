adf2d5015fe76a01782a9e20986756f065d0fb4f
kenware <ejykken@gmail.com>
2021-05-30 12:56:57 +0100

    add changes

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:htt
 app.use('/api/v1', Route);
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, '/client')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, './client/index.html')));
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './client/build/index.html')));
 app.listen(PORT, () => logger.info(`App is running at port ${PORT}`));
 
 export default app;
